f84ff7355c7f231606cf3e5041489b4461bd4226
James Cooper <jarak_jakar@hotmail.com>
2016-05-25 21:11:30 +1200

    Nothing much.

--- 773-A4/773-A4.cpp
+++ 773-A4/773-A4.cpp
@@ -1,12 +1,25 @@
 // 773-A4.cpp : Defines the entry point for the console application.
-//
+// This is a naive implementation, with no optimisations for multi-threading or using SIMD instructions
 
 #include "stdafx.h"
-#include "CImg.h"
+#include "FreeImagePlus.h"
 
 
-int main()
+int main(int argc, char *argv[])
 {
-    return 0;
+	fipImage leftPic, rightPic;
+	
+	if (!leftPic.load(argv[1])) {
+		return -1;
+	}
+
+	if (!rightPic.load(argv[2])) {
+		return -1;
+	}
+
+	// Build averaging of the right image
+
+
+	return 0;
 }
 
