e7dc86a03a3df67b76398bcda9ebfe8d38ba19dc
J C <jarak_jakar@hotmail.com>
2016-05-31 12:50:36 +1200

    Continued work on implementing rectification

--- ConsoleApplication1/773-A4-Rect.cpp
+++ ConsoleApplication1/773-A4-Rect.cpp
@@ -21,20 +21,107 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	ifstream leftPicCalibrationParamsFile;
-	ifstream rightPicCalibrationParamsFile;
+	ifstream calibrationParamsFile;
 
-	leftPicCalibrationParamsFile.open(argv[3]);
-	Mat<double> leftCameraMatrix(3, 3);
-	Mat<double> leftRotationMatrix(3, 4);
+	calibrationParamsFile.open(argv[3]);
+	Mat<double> leftCameraMatrix(3, 3, fill::zeros);
+	Mat<double> leftRotationTranslationMatrix(3, 4, fill::zeros);
+	Mat<double> rightCameraMatrix(3, 3, fill::zeros);
+	Mat<double> rightRotationTranslationMatrix(3, 4, fill::zeros);
 
-	if (leftPicCalibrationParamsFile.is_open()) {
+	// Read in calibration parameters
+	if (calibrationParamsFile.is_open()) {
 		string line;
-		for (int i = 0; i < 3; i++) {
-			getline(leftPicCalibrationParamsFile, line);
 
+		//Do the left camera's stuff
+		for (int i = 0; i < 3; i++) {
+			getline(calibrationParamsFile, line);
+
+			string token = line.substr(0, line.find(","));
+			leftCameraMatrix(i, 0) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			leftCameraMatrix(i, 1) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			leftCameraMatrix(i, 2) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			leftRotationTranslationMatrix(i, 0) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			leftRotationTranslationMatrix(i, 1) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			leftRotationTranslationMatrix(i, 2) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			leftRotationTranslationMatrix(i, 3) = stod(token);
+		}
+
+		getline(calibrationParamsFile, line);
+
+		// Do the right camera's stuff
+		for (int i = 0; i < 3; i++) {
+			getline(calibrationParamsFile, line);
+
+			string token = line.substr(0, line.find(","));
+			rightCameraMatrix(i, 0) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			rightCameraMatrix(i, 1) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			rightCameraMatrix(i, 2) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			rightRotationTranslationMatrix(i, 0) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			rightRotationTranslationMatrix(i, 1) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			rightRotationTranslationMatrix(i, 2) = stod(token);
+			line.erase(0, (line.find(",") + 1));
+
+			string token = line.substr(0, line.find(","));
+			rightRotationTranslationMatrix(i, 3) = stod(token);
 		}
 	}
+
+	calibrationParamsFile.close();
+
+	// Find optical centres for the cameras
+
+	Mat<double> leftProjectionMatrix = leftCameraMatrix * leftRotationTranslationMatrix;
+	Mat<double> rightProjectionMatrix = rightCameraMatrix * rightRotationTranslationMatrix;
+	Col<double> leftOpticalCentre = (leftProjectionMatrix.cols(0, 2)).i() * leftProjectionMatrix.col(3);
+	Col<double> rightOpticalCentre = (rightProjectionMatrix.cols(0, 2)).i() * rightProjectionMatrix.col(3);
+
+	Col<double> v1 = leftOpticalCentre - rightOpticalCentre;
+	//Mat<double> leftRotationMatrix = leftRotationTranslationMatrix.head_cols(3);
+	//Row<double> R3 = leftRotationMatrix.row(2);
+	//Col<double> R3T = R3.t();
+	Col<double> v2 = cross((((leftRotationTranslationMatrix.head_cols(3)).row(2)).t()), v1);
+	Col<double> v3 = cross(v1, v2);
+
+	normalise(v1);
+	normalise(v2);
+	normalise(v3);
+
+
+
 
     return 0;
 }
